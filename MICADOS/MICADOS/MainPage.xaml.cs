6e1b91642b40b0f685f6e4bace6e0fb415a41e9f
canat.paul <canat.paul@gmail.com>
2020-10-02 21:45:51 +0200

    marchandise

--- MICADOS/MICADOS/MainPage.xaml.cs
+++ MICADOS/MICADOS/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 
 namespace MICADOS
 {
@@ -14,11 +15,32 @@ namespace MICADOS
         public MainPage()
         {
             InitializeComponent();
+            string tag = "micados";
             ObservableCollection<string> itemList;
             itemList = new ObservableCollection<string>();
             itemList.Add("je suis à vendre !");
-            itemsDepenses.ItemsSource = itemList;
+            itemsRecettes.ItemsSource = itemList;
+            Marchandise m = new Marchandise("coca", 1.2, 4);
+            List<Marchandise> l = new List<Marchandise>();
+            l.Add(m);
+            ListeMarchandises lm = new ListeMarchandises(l);
+            lm.SaveMarchandises();
+            Log.Warning(tag, lm.toString());
+            ListeMarchandises listmar = new ListeMarchandises();
+            listmar.ReadMarchandises();
+            ObservableCollection<string> cocalist = new ObservableCollection<string>();
+            for(int i = 0; i < listmar.listM.Count; i++)
+            {
+                cocalist.Add(listmar.listM[i].nom);
+            }
+            Log.Warning(tag, listmar.toString());
+            itemsRecettes.ItemsSource = cocalist;
+        }
 
+        private void itemsRecettes_itemtapped(object sender, FocusEventArgs e)
+        {
+            /// TODO
+            /// itemsVenteEnCours.ItemsSource
         }
     }
 }
---
+++ MICADOS/MICADOS/Marchandise.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Xamarin.Forms;
+using System.Linq;
+using System.Globalization;
+using System.Xml;
+using System.Data;
+using Xamarin.Forms.Markup;
+
+namespace MICADOS
+{
+    class Marchandise
+    {
+        public string nom;
+        public double prix;
+        public int stock;
+        public Marchandise(string n, double p, int s)
+        {
+            nom = n;
+            prix = p;
+            stock = s;
+        }
+
+        public Marchandise(string n, string p, string s)
+        {
+            nom = n;
+            stock = Int32.Parse(s);
+            prix = float.Parse(p, CultureInfo.InvariantCulture.NumberFormat);
+        }
+
+        public string toString()
+        {
+            return ("nom : " + nom + ", prix : " + prix + ", stock : " + stock);
+        }
+
+    }
+    class ListeMarchandises
+    {
+        public List<Marchandise> listM = new List<Marchandise>();
+
+        public ListeMarchandises(){}
+        public ListeMarchandises(List<Marchandise> l)
+        {
+            for(int i = 0; i < l.Count; i++)
+            {
+                listM.Add(l[i]);
+            }
+        }
+
+        public string toString()
+        {
+            string ret = "";
+            for(int i = 0 ; i < listM.Count; i++)
+            {
+                ret = ret + listM[i].toString() + "\n";
+            }
+            return ret;
+        }
+
+        public void addMarchandise(Marchandise m)
+        {
+            listM.Add(m);
+        }
+
+        public void ReadMarchandises()
+        {
+            var dataPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "data.micados");
+            using (var reader = new StreamReader(dataPath, true))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] mar = line.Split(',');
+                    string n = mar[0];
+                    string p = mar[1];
+                    string s = mar[2];
+                    listM.Add(new Marchandise(n, p, s));
+                    for(int i = 0; i < listM.Count; i++)
+                    {
+                        Xamarin.Forms.Internals.Log.Warning("micados", listM[i].ToString());
+                    }
+                }
+
+            }
+        }
+        public void SaveMarchandises()
+        {
+            var dataPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "data.micados");
+            using (var writer = File.CreateText(dataPath))
+            {
+                for (int i = 0; i < listM.Count; i++)
+                {
+                    writer.WriteLine(listM[i].nom.ToString() + "," + listM[i].prix.ToString() + "," + (string)listM[i].stock.ToString());
+                }
+            }
+        }
+    }
+}
+
